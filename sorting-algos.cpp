e5fca846d2dbab7bd31c775db53c26497352614e
ndevbogado <ndevbogado@gmail.com>
2022-11-10 08:46:18 -0300

    Create sorting-algos.cpp

---
+++ sorting-algos.cpp
@@ -0,0 +1,22 @@
+// This is a comprehesive list of the most known sorting algorithms
+
+//BUBBLE SORT:
+// The simplest in terms of code but it's the most inefficient: The number of iterations can be much higher than the elements to sort.
+
+
+//SELECTION SORT:
+
+
+//QUICK SORT:
+
+
+
+// OTHER (COMPLEX) SORTING ALGORITHMS: 
+/*
+	INSERTION SORT -------- Improved version of selection sort.
+	MODIFIED BUBBLE SORT --	Improved version of bubble sort.
+	EXCHANGE SORT --------- Similar to bubble sort, but compares elements to the first value.
+	SHELL SORT ------------ Similar to bubble sort, but compares elements that are a certain distance away from each other.
+	HEAP SORT -------------	Similat to selection sort, but uses a 'heap' data structure for storage.
+	MERGE SORT ------------ Similar to quicksort, but combines sorted arrays (partitions) and can be used effectively with linked lists.
+*/
